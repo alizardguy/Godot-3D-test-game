450c0b8db092c0fd19f2277b69e82168ff338cfe
alizardguy <42082829+alizardguy@users.noreply.github.com>
2022-10-25 17:59:21 -0700

    Funny gravity error

--- src/Prefabs/Player/Avatar.gd
+++ src/Prefabs/Player/Avatar.gd
@@ -23,10 +23,38 @@ func _ready():
 	# link slots
 	head = $head
 	#hides the cursor
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED);
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
-		head.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity))
-		head.rotation.x = clamp(head.rotation.x, deg2rad(-89), deg2rad(89))
+		rotate_y(deg2rad(-event.relative.x * mouse_sensitivity));
+		head.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity));
+		head.rotation.x = clamp(head.rotation.x, deg2rad(-89), deg2rad(89));
+
+func _process(delta):
+	direction = Vector3();
+	
+# gravity
+	if is_on_floor() or is_on_ceiling():
+		fall.y = gravity
+	else:
+		fall.y -= gravity * delta
+# tab out
+	if Input.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE);
+# movement
+	if Input.is_action_just_pressed("move_jump"):
+		fall.y = jump;
+	if Input.is_action_pressed("move_forward"):
+		direction -= transform.basis.z;
+	elif Input.is_action_pressed("move_backward"):
+		direction += transform.basis.z;
+	if Input.is_action_pressed("move_left"):
+		direction -= transform.basis.x;
+	elif Input.is_action_pressed("move_right"):
+		direction += transform.basis.x;
+# apply direction
+	direction = direction.normalized();
+	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta);
+	velocity = move_and_slide(velocity, Vector3.UP);
+	move_and_slide(fall, Vector3.UP);
