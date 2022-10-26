c93b56a71f879bf483e54b1ab0c6dbc8d45ac08b
alizardguy <42082829+alizardguy@users.noreply.github.com>
2022-10-25 17:32:09 -0700

    Generic head movement script
    It is a player controller of all time

--- src/Prefabs/Player/Avatar.gd
+++ src/Prefabs/Player/Avatar.gd
@@ -1,16 +1,32 @@
 extends KinematicBody
 
+### vars
+# player move vars
+var speed = 7;
+var acceleration = 20;
+var gravity = 9.8;
+var jump = 5;
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# input setting vars
+var mouse_sensitivity = 0.05;
 
+# storage of vectors
+var direction = Vector3();
+var velocity = Vector3();
+var fall = Vector3();
+
+#refs
+var head = null;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# link slots
+	head = $head
+	#hides the cursor
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
+		head.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity))
+		head.rotation.x = clamp(head.rotation.x, deg2rad(-89), deg2rad(89))
