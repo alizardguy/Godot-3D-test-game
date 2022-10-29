25ecfc446901fbc817b6c2e4a2346e2edfc75671
alizardguy <42082829+alizardguy@users.noreply.github.com>
2022-10-28 21:54:42 -0700

    omg door hiiiiiiiiii
    :D

--- src/Scripts/objects/Interaction-objects/basicDoor.gd
+++ src/Scripts/objects/Interaction-objects/basicDoor.gd
@@ -1,10 +1,19 @@
 extends Spatial
 
+var isOpen:bool = false;
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(get_node("/root/Root/Avatar/head/InteractRaycast"));
 	pass # Replace with function body.
 
 func player_interact():
-	print("a player interacted with me");
+	var tween = get_node("door pivot/Tween");
+	var current = $"door pivot".rotation_degrees
+	if isOpen == false:
+		tween.interpolate_property($"door pivot", "rotation_degrees", current, Vector3(0,90,0), 0.1, Tween.TRANS_LINEAR, Tween.TRANS_LINEAR);
+		isOpen = true;
+	else:
+		tween.interpolate_property($"door pivot", "rotation_degrees", current, Vector3(0,0,0), 0.1, Tween.TRANS_LINEAR, Tween.TRANS_LINEAR);
+		isOpen = false;
+	tween.start();
 	pass
